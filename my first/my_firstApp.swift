57b02bc0577ff7568144a3a4d8121f0d63e3c7cc
Humphrey Hanson <humphreyhanson@icloud.com>
2023-05-05 19:42:02 +0200

    Initial Commit

---
+++ my first/my_firstApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_firstApp.swift
+//  my first
+//
+//  Created by Humphrey Hanson on 05.05.23.
+//
+
+import SwiftUI
+
+@main
+struct my_firstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
